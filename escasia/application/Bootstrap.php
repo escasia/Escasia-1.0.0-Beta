2b8c51af2667cf6019e9f5a983f747ecae2ce90c
tvu <toan.vu@snc-design.de>
2012-12-02 20:06:57 +0100

    update

--- escasia/application/Bootstrap.php
+++ escasia/application/Bootstrap.php
@@ -1,5 +1,13 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initView()
+	{
+	    $view = new Zend_View();
+	    $view->doctype('XHTML1_STRICT');
+	    // do other stuff to the view...
+	    return $view;
+	}
+	
 }
 
